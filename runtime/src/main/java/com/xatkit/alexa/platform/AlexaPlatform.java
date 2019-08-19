819912adbf936d1dda3f5d918db4e3648fd5ddb4
Luca Baruffini <luca.baruffini@mail.polimi.it>
2019-08-19 19:41:12 +0200

    Added Reply action and basic logging

--- runtime/src/main/java/com/xatkit/alexa/platform/AlexaPlatform.java
+++ runtime/src/main/java/com/xatkit/alexa/platform/AlexaPlatform.java
@@ -1,5 +1,6 @@
 package com.xatkit.alexa.platform;
 
+import com.xatkit.alexa.AlexaUtils;
 import com.xatkit.core.XatkitCore;
 
 import com.xatkit.plugins.chat.platform.ChatPlatform;
@@ -15,16 +16,28 @@ import org.apache.commons.configuration2.Configuration;
  * This class is part of xatkit's core platforms, and can be used in an execution model by importing the
  * <i>AlexaPlatform</i> package.
  */
-public class AlexaPlatform extends ChatPlatform {
+public class AlexaPlatform extends ChatPlatform {	
 
+	//Alexa keychain
+	private AlexaUtils alexaUtils;
+	
 	public AlexaPlatform(XatkitCore xatkitCore, Configuration configuration) {
 		super(xatkitCore,configuration);
+		
+		//LOGGING
 		Log.info("Alexa core service started");
+		
+		//Registers a REST Endpoint to respond to alexa requests
 		this.getXatkitCore().getXatkitServer().registerRestEndpoint("/alexa/receiver", 
 		 (headers, param, content) -> {
+			 	
+			 	//Retrieves request body content
                 JsonObject contentObject = content.getAsJsonObject();
-                String contentWhole = contentObject.getAsString();
-                Log.info("Content: {0}", contentWhole);
+                
+                //LOGGING
+                Log.info("Content: {0}", contentObject.getAsString());
+                
+                //Builds a 
                 JsonObject result = new JsonObject();
                 return result;
             });
---
+++ runtime/src/main/java/com/xatkit/alexa/platform/action/Reply.java
@@ -0,0 +1,20 @@
+package com.xatkit.alexa.platform.action;
+
+import com.xatkit.alexa.platform.AlexaPlatform;
+import com.xatkit.core.platform.action.RuntimeAction;
+import com.xatkit.core.session.XatkitSession;;
+
+public class Reply extends RuntimeAction<AlexaPlatform> {
+
+	public Reply(AlexaPlatform runtimePlatform, XatkitSession session) {
+		super(runtimePlatform, session);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Object compute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
