f094658e02b59627389820ebc96ab161ee46028d
Luca Baruffini <luca.baruffini@mail.polimi.it>
2019-08-19 18:36:05 +0200

    Added key to utilities for alexa

--- runtime/src/main/java/com/xatkit/alexa/AlexaUtils.java
+++ runtime/src/main/java/com/xatkit/alexa/AlexaUtils.java
@@ -7,4 +7,7 @@ public interface AlexaUtils extends ChatUtils {
 	String ALEXA_API_KEY = "xatkit.alexa.api.key";
 	
 	String ALEXA_CONTEXT_KEY = "alexa";
+	
+	String ALEXA_INVOCATION_MESSAGE_KEY = "alexa.invocation.message"; //Key to retrieve default invocation welcome message
+	
 }
