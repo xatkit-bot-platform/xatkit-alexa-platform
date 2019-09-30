d7fdc311430b1b064720ad89b558db3d68dab9fe
Luca Baruffini <luca.baruffini@mail.polimi.it>
2019-09-30 18:51:56 +0200

    Fixes and alignments to xatkit framework

--- runtime/src/main/java/com/xatkit/alexa/AlexaUtils.java
+++ runtime/src/main/java/com/xatkit/alexa/AlexaUtils.java
@@ -14,11 +14,11 @@ public interface AlexaUtils extends ChatUtils {
 	
 	String ALEXA_USERID_CONTEXT_KEY = "user_id";
 
-	String ALEXA_INVOCATION_MESSAGE_KEY = "alexa.invocation.message"; //Key to retrieve default invocation welcome message
+	String ALEXA_INVOCATION_MESSAGE_KEY = "xatkit.alexa.invocationMessage"; //Key to retrieve default invocation welcome message
 
 	String DEFAULT_ALEXA_INVOCATION_MESSAGE = "Hello! Welcome to Xatkit! What can I do for you?";
 
-	String ALEXA_RESPONSE_NOT_FOUND_MESSAGE_KEY = "alexa.response.not.found.message"; //Key to retrieve default message to send to Alexa
+	String ALEXA_RESPONSE_NOT_FOUND_MESSAGE_KEY = "xatkit.alexa.responseNotFoundMessage"; //Key to retrieve default message to send to Alexa
 	
 	String DEFAULT_ALEXA_RESPONSE_NOT_FOUND_MESSAGE = "Xatkit could not respond to your request, can you please try again?";
 }
--- runtime/src/main/java/com/xatkit/alexa/platform/action/Reply.java
+++ runtime/src/main/java/com/xatkit/alexa/platform/action/Reply.java
@@ -1,5 +1,9 @@
 package com.xatkit.alexa.platform.action;
 
+import static java.util.Objects.nonNull;
+
+import javax.annotation.Nonnull;
+
 import com.xatkit.alexa.AlexaUtils;
 import com.xatkit.alexa.platform.AlexaPlatform;
 import com.xatkit.core.platform.action.RuntimeMessageAction;
@@ -18,9 +22,13 @@ public class Reply extends RuntimeMessageAction<AlexaPlatform> {
 	@Override
 	protected Object compute() throws Exception {
 		// The request is retrieved from the new context created from the sessionId
-		String requestId = (String) this.session.getRuntimeContexts().getContextValue(this.sessionID, AlexaUtils.ALEXA_REQUEST_ID_CONTEXT_KEY);
-		Log.info("Request found: {0}", requestId);
-		this.runtimePlatform.storeMessage(requestId, message);
+		if(nonNull(this.sessionID)) {
+			String requestId = (String) this.session.getRuntimeContexts().getContextValue(this.sessionID, AlexaUtils.ALEXA_REQUEST_ID_CONTEXT_KEY);
+			Log.info("Request found: {0}", requestId);
+			if(nonNull(requestId)) {
+				this.runtimePlatform.storeMessage(requestId, message);
+			}
+		}
 		return null;
 	}
 
@@ -34,6 +42,6 @@ public class Reply extends RuntimeMessageAction<AlexaPlatform> {
 		 */
 		this.sessionID = (String) this.session.getRuntimeContexts().getContextValue(AlexaUtils.ALEXA_CONTEXT_KEY, AlexaUtils.ALEXA_SESSION_ID_CONTEXT_KEY);
 		Log.info("Session found: {0}", sessionID);
-		return this.runtimePlatform.getXatkitCore().getOrCreateXatkitSession(this.sessionID);
+		return nonNull(this.sessionID) ? this.runtimePlatform.getXatkitCore().getOrCreateXatkitSession(this.sessionID) : null;
 	}
 }
